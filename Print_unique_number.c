3ab7f57fcbf461d088e287f73ad61463756b681b
kaisen1337 <kaisenkaisen100@gmail.com>
2024-12-24 07:41:22 +0100

    Update Print_unique_number.c

--- Print_unique_number.c
+++ Print_unique_number.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-
-int main()
+int	main(void)
 {
-    int n;
-    int r;
-    int arr[10]={0};
-    scanf("%d", &n);
-    int counter = 0;
-    while (n)
-    {
-        r = n%10;
-        arr[r] += 1;
-        n = n/10;
-    }
-    int i = 0;
-    while(i <= 10)
-    {
-        if(arr[i] > 0)
-            counter++;
-        i++;
-    }
-        printf("%d", counter);
-        return 0;
+	long n;
+	int r;
+	int arr[10] = {0};
+	scanf("%ld", &n);
+	int counter = 0;
+	while (n)
+	{
+		r = n % 10;
+		if (arr[r] == 0)
+			arr[r] += 1;
+		n = n / 10;
+	}
+	int i = 0;
+	while (i <= 10)
+	{
+		if (arr[i] == 1)
+			counter++;
+		i++;
+	}
+	printf("%d", counter);
+	return (0);
 }
